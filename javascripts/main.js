10c5ba90ccc9b09670f759ff6643807a38d2a773
Gustav Solis <guso9085@colorado.edu>
2016-11-19 17:48:07 -0700

    Banners

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $('.button').click(function(e) {
     e.preventDefault();
-    $.get('https://crossorigin.me/http://talk-to-obama.herokuapp.com/chat')
+    $.get('http://talk-to-obama.herokuapp.com/chat')
       .then(function(d){
-        $('.talk').text('"'+ d.content + '"');
+        $('.talk').text('"' + d.content + '"');
       })
   });
 });
