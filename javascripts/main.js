c0b5f4ae7f96599f1de40df7575c43054b00eff1
Gustav Solis <guso9085@colorado.edu>
2016-11-19 18:03:01 -0700

    Loading

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   $('.button').click(function(e) {
     e.preventDefault();
+    $('.loading').addClass("show")
     $.get('http://talk-to-obama.herokuapp.com/chat')
       .then(function(d){
+        $('.loading').removeClass("show")
         $('.talk').text('"' + d.content + '"');
       })
   });
