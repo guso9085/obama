42d14a65bd67109398334ab4121fa1eca6e9482f
Gustav Solis <guso9085@colorado.edu>
2016-11-19 19:13:52 -0700

    Final touches

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   $('.button').click(function(e) {
     e.preventDefault();
     $('.button').html("<img src='loading.gif'/>");
-    $.get('http://talk-to-obama.herokuapp.com/chat')
+    $.get('http://talk-to-obama.herokuapp.com/chat?size=tweet')
       .then(function(d){
         $('.button').html("<h1 id='buttonT'> SAUCE ME </h1>");
         $('.talk').text('"' + d.content + '"');
