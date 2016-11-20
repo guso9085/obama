a4918f45d384f4ab2566113d89c19f3b9ed67412
Gustav Solis <guso9085@colorado.edu>
2016-11-19 17:30:41 -0700

    Debugging

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('.button').click(function(e) {
     e.preventDefault();
-    $.get('http://cors-enabler.herokuapp.com/http://talk-to-obama.herokuapp.com/chat')
+    $.get('https://crossorigin.me/http://talk-to-obama.herokuapp.com/chat')
       .then(function(d){
         $('.talk').text(d);
       })
